10ad0878bc2c6297f3bb7a24fdc56ab09c95d1c7
seb <bastman77@googlemail.com>
2012-11-12 12:05:49 +0100

    added memorylimit, pagesze options to Task "Dump Couchbase" as xml and json

--- example/teststack/TestStackExample/Task/Couchbase/Admin/Bucket/Dump.php
+++ example/teststack/TestStackExample/Task/Couchbase/Admin/Bucket/Dump.php
@@ -71,12 +71,18 @@ class Dump
      */
     public function run()
     {
+
+        // got good results with: memorylimit: 3000M and pageSize: 30000
+
+
         echo (
             PHP_EOL . '==========' . __METHOD__ . ' :BEGIN ===========' . PHP_EOL
         );
+        echo (
+            PHP_EOL . 'Recommended: MemoryLimit 3000M pageSize 30000' . PHP_EOL
+        );
 
         $this->init();
-
 
         $client = $this->_getCouchbaseAdminClient();
         $client->setHttpClientVerbose(false);
@@ -86,50 +92,71 @@ class Dump
         $password = 'Administrator';
         $bucket = 'default';
 
+        $recordSetPagesSize = 1000;
+        // the higher the pageSize, the more moery you need
+        $memoryLimit = ini_get('memory_limit');
 
         $host = $this->_cliPromptInput(
             'Host=' . $host . ':',
             false,
             $host
         );
-
+        echo PHP_EOL . 'host=' . json_encode($host) . PHP_EOL;
         $restApiPort = $this->_cliPromptInput(
             'RestApiPort=' . $restApiPort . ':',
             false,
             $restApiPort
         );
+        echo PHP_EOL . 'restApiPort=' . json_encode($restApiPort) . PHP_EOL;
         $username = $this->_cliPromptInput(
             'Username=' . $username . ':',
             false,
             $username
         );
+        echo PHP_EOL . 'username=' . json_encode($username) . PHP_EOL;
         $password = $this->_cliPromptInput('Password:', true, $password);
+        echo PHP_EOL . 'password= (secret. not displayed here)'. PHP_EOL;
         $bucket = $this->_cliPromptInput(
             'Bucket=' . $bucket . ':',
             false,
             $bucket
         );
+        echo PHP_EOL . 'bucket=' . json_encode($bucket) . PHP_EOL;
         $client->setAuthEnabled($bucket !=='default');
+        echo PHP_EOL . 'authEnabled='
+            . json_encode($client->getAuthEnabled()) . PHP_EOL;
+
+        $recordSetPagesSize = (int)$this->_cliPromptInput(
+            'recordSetPageSize=' . $recordSetPagesSize . ':',
+            false,
+            $bucket
+        );
+        echo PHP_EOL . 'recordSetPageSize='
+            . json_encode($recordSetPagesSize) . PHP_EOL;
+
+        $memoryLimit = $this->_cliPromptInput(
+            'memoryLimit=' . $memoryLimit . ':',
+            false,
+            $memoryLimit
+        );
+        ini_set('memory_limit', $memoryLimit);
+        $memoryLimit = ini_get('memory_limit');
+        echo PHP_EOL . 'memoryLimit=' . json_encode($memoryLimit) . PHP_EOL;
 
         $client->setHost($host);
         $client->setRestApiPort($restApiPort);
         $client->setUsername($username);
         $client->setPassword($password);
 
-
         $this->createFilesystemCache();
-
 
         $cacheDirJsonFileInfo = $this->getCacheDirJsonFileInfo();
         $cacheDirXmlFileInfo = $this->getCacheDirXmlFileInfo();
 
-
-        $pageSize = 1000;
+        $pageSize = $recordSetPagesSize;
         $offset = 0;
 
         $totalRows = 0;
-        // $allRows = array();
-
 
         $params = array(
             'limit' => $pageSize,
@@ -139,19 +166,24 @@ class Dump
         $pageNo = 1;
         $rowsCounter = 0;
 
-
         $jsonFileInfo = new \SplFileInfo(
             $cacheDirJsonFileInfo->getRealPath() . '/couchbasedump.jsonx'
         );
-
-
 
         $xmlFileInfo = new \SplFileInfo(
             $cacheDirXmlFileInfo->getRealPath() . '/couchbasedump.xml'
         );
 
+        echo PHP_EOL . 'Files (json) to be stored = '
+            . $jsonFileInfo->getPathname() . PHP_EOL;
+
+        echo PHP_EOL . 'Files (xml) to be stored = '
+            . $xmlFileInfo->getPathname() . PHP_EOL;
         $jsonFileResource = fopen($jsonFileInfo->getPathname(), 'w+');
         $xmlFileResource = fopen($xmlFileInfo->getPathname(), 'w+');
+
+        echo PHP_EOL . 'Calling couchbase rest api ... please wait ...'
+           . PHP_EOL;
 
         while (true) {
             $params['limit'] = $pageSize;
@@ -162,6 +194,9 @@ class Dump
                 $totalRows = $data['total_rows'];
             }catch (\Exception $e){
 
+                /**
+                 * @var $client \Processus\Couchbase\Admin\Client
+                 */
                 var_dump($client->getLastResponseInfo());
                 throw $e;
             }
@@ -173,8 +208,10 @@ class Dump
                     'rowsCount' => count($rows),
                     'pageNo' => $pageNo,
                     'params' => $params,
-                    'memUsage'=>memory_get_usage(true),
-                    'memPeakUsage'=>memory_get_peak_usage(true),
+                    'memUsage' => memory_get_usage(false),
+                    'memUsageReal' => memory_get_usage(true),
+                    'memPeakUsage' => memory_get_peak_usage(false),
+                    'memPeakUsageReal' => memory_get_peak_usage(true),
 
                 )
             );
@@ -190,25 +227,18 @@ class Dump
             }
 
             foreach ($rows as $row) {
-                //$allRows[] = $rows;
                 $jsonText = json_encode($row)
                     . PHP_EOL . PHP_EOL . PHP_EOL;
 
                 fwrite($jsonFileResource, $jsonText);
-
 
                 $xmlSerializer = new XmlRpcValue();
                 $xmlSerializer->setEncoding('UTF-8');
                 $xmlText = $xmlSerializer->encode($row)
                     . PHP_EOL . PHP_EOL . PHP_EOL;
 
-
                 fwrite($xmlFileResource, $xmlText);
-
-
             }
-
-
 
             $offset = $offset + (int)count($rows);
             $pageNo++;
@@ -226,7 +256,6 @@ class Dump
         fclose($xmlFileResource);
 
         $totalRowsCount = $totalRows;
-        // $allRowsCount = count($allRows);
         var_dump(
             array(
                 'totalRowsCount' => $totalRowsCount,
@@ -235,14 +264,13 @@ class Dump
             )
         );
         //var_dump($client->getLastResponseInfo());
-
-
     }
 
-
+    /**
+     * @throws \Exception
+     */
     private function createFilesystemCache()
     {
-
         $cacheDirFileInfo = $this->getCacheDirFileInfo();
         //   var_dump($cacheDirFileInfo->getPathname());exit;
         if (!$cacheDirFileInfo->isDir()) {
@@ -260,14 +288,17 @@ class Dump
             );
         }
 
-        $uuid = str_pad(time(), 20, '0');
+        $host = $this->_getCouchbaseAdminClient()->getHost();
+
+        $uuid = 'dump_' . urlencode($host)
+            . '_' . str_pad(time(), 20, '0', STR_PAD_LEFT);
         $cacheDirJson = $cacheDirFileInfo->getPathname()
             . '/' . str_replace(
             array('::', '\\'),
             '_',
             strtolower(__METHOD__)
                 . '_' . $uuid
-                . '_json'
+                . ''
         );
         $cacheDirXml = $cacheDirFileInfo->getPathname()
             . '/' . str_replace(
@@ -275,7 +306,7 @@ class Dump
             '_',
             strtolower(__METHOD__)
                 . '_' . $uuid
-                . '_xml'
+                . ''
         );
         $cacheDirJsonFileInfo = new \SplFileInfo($cacheDirJson);
         $this->cacheDirJsonFileInfo = $cacheDirJsonFileInfo;
